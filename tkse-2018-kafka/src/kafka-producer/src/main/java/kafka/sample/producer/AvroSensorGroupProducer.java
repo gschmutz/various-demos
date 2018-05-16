a33b1e537e0178d021f5080bca1babe4531a03fa
Guido Schmutz <guido.schmutz@hotmail.com>
2018-05-16 18:05:05 +0200

    update latest version

--- tkse-2018-kafka/src/kafka-producer/src/main/java/kafka/sample/producer/AvroSensorGroupProducer.java
+++ tkse-2018-kafka/src/kafka-producer/src/main/java/kafka/sample/producer/AvroSensorGroupProducer.java
@@ -23,67 +23,54 @@ public class AvroSensorGroupProducer {
 
 	private Producer<String, SensorGroupOne> connect() {
 		Producer<String, SensorGroupOne> producer = null;
-    	
+
 		Properties props = new Properties();
-	    props.put("bootstrap.servers", "localhost:9092");
-	    props.put("acks", "all");
-	    props.put("retries", 0);
-	    props.put("key.serializer", "io.confluent.kafka.serializers.KafkaAvroSerializer");
-	    props.put("value.serializer", "io.confluent.kafka.serializers.KafkaAvroSerializer");
-	    props.put("schema.registry.url", "http://localhost:8081");
+		props.put("bootstrap.servers", "localhost:9092");
+		props.put("acks", "all");
+		props.put("retries", 0);
+		props.put("key.serializer", "io.confluent.kafka.serializers.KafkaAvroSerializer");
+		props.put("value.serializer", "io.confluent.kafka.serializers.KafkaAvroSerializer");
+		props.put("schema.registry.url", "http://localhost:8081");
 
 		try {
-    		producer = new KafkaProducer<String, SensorGroupOne>(props);
-    	} catch (Exception e) {
-    		e.printStackTrace();
-    	}
-    	return producer;
-	}
-	
-	public void produce(SensorGroupOne value) throws IOException {
-        final Random rnd = new Random();
-
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-
-        DatumWriter<SensorGroupOne> writer = new SpecificDatumWriter<SensorGroupOne>(SensorGroupOne.class);
-        Encoder encoder = EncoderFactory.get().binaryEncoder(out, null);
-        //Encoder encoder = EncoderFactory.get().jsonEncoder(SensorGroupOne.getClassSchema(), out);
-        
-        try {
-			writer.write(value, encoder);
-	        encoder.flush();
-	        out.close();
-		} catch (IOException ioe) {
-			throw new RuntimeException(ioe);
+			producer = new KafkaProducer<String, SensorGroupOne>(props);
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-        
-        System.out.write(out.toByteArray());
-        
-        if (producer == null) {
-//        		producer = connect();
-        }
-        
-        Integer key = rnd.nextInt(255);
-        
-        ProducerRecord<String, SensorGroupOne> record = new ProducerRecord<String, SensorGroupOne>(kafkaTopicSensorGroupOne, null, value);
+		return producer;
+	}
 
-        if (producer !=null) {
-        	try {
-        		Future<RecordMetadata> future = producer.send(record);
-        		RecordMetadata metadata = future.get();
-        	} catch (Exception e) {
-        		System.err.println(e.getMessage());
-        		e.printStackTrace();
-        	}
-        	
-        }
-		
+	public void produce(SensorGroupOne value) throws IOException {
+		final Random rnd = new Random();
+
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+		if (producer == null) {
+			producer = connect();
+		}
+
+		Integer key = rnd.nextInt(255);
+
+		ProducerRecord<String, SensorGroupOne> record = new ProducerRecord<String, SensorGroupOne>(
+				kafkaTopicSensorGroupOne, null, value);
+
+		if (producer != null) {
+			try {
+				Future<RecordMetadata> future = producer.send(record);
+				RecordMetadata metadata = future.get();
+			} catch (Exception e) {
+				System.err.println(e.getMessage());
+				e.printStackTrace();
+			}
+
+		}
+
 	}
 
 	public static void main(String[] args) {
 		AvroSensorGroupProducer producer = new AvroSensorGroupProducer();
-		
-		SensorGroupOne value = new SensorGroupOne(0.1d,0.2d);
+
+		SensorGroupOne value = new SensorGroupOne(0.1d, 0.2d);
 		try {
 			producer.produce(value);
 		} catch (IOException e) {
